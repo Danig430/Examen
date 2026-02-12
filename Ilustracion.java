55d2bb69999770a494635a0b5857ef672856417b
danig430 <danig430@gmail.com>
2026-02-12 12:27:40 +0100

    eso2

--- Ilustracion.java
+++ Ilustracion.java
@@ -2,7 +2,7 @@ public static void multiplos(int numeroMax){
 
     for(int i=0; i<= numeroMax; i++){
         if(i%7==0){ 
-            System.out.println("es multiplos de 7: "+ i + 2); 
+            System.out.println("eso multiplos de 7: "+ i + 2); 
         }
         if(i%49==0){
             System.out.println("Multiplos de 49: "+i);
